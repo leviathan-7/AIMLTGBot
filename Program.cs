b61d3149707c9d0ba423c559f70d2d06b3ac1ce8
leviathan-7 <m.grober02@yandex.ru>
2022-12-15 20:08:30 +0300

    Телеграмм бот для смайликов

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AIMLTGBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = System.IO.File.ReadAllText("TGToken.txt");
+            using (var tg = new TelegramService(token, new AIMLService()))
+            {
+                Console.WriteLine($"Подключились к телеграмму как бот { tg.Username }. Ожидаем сообщений. Для завершения работы нажимте Enter");
+                Console.ReadLine();
+            }
+        }
+    }
+}
