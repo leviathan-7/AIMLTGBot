8e6138083c84861c2b9c1475634c382ef4308ca9
tediorewf <meremeev@sfedu.ru>
2022-12-23 19:22:26 +0300

    Обновил образы в AIML

--- ResultFromNeyronNetwork.cs
+++ ResultFromNeyronNetwork.cs
@@ -17,32 +17,34 @@ namespace AIMLTGBot
         public string Result(Bitmap img)
         {
             double[] S = ArrFromBitmap.ToArr(img);
-            double[]  res = Network.Compute(S);
+            //double[] res = Network.Compute(S);
             int j = 0;
             double max = 0;
-            for (int i = 0; i < res.Length; i++)
-                if (res[i]>max)
+            /*for (int i = 0; i < res.Length; i++)
+            {
+                if (res[i] > max)
                 {
                     max = res[i];
                     j = i;
                 }
+            }*/
             string str = "";
             switch (j)
             {
                 case 0:
-                str = "улыбка";
+                    str = "улыбка";
                     break;
                 case 1:
-                str = "любовь";
+                    str = "грусть";
                     break;
                 case 2:
-                str = "нейтральный";
+                    str = "нейтральный";
                     break;
                 case 3:
-                str = "сонный";
+                    str = "удивление";
                     break;
                 case 4:
-                str = "злой";
+                    str = "злой";
                     break;
                 default:
                     break;
